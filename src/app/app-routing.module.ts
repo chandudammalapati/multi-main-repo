6a2aed4b212e87925e938c62b41efff9eb83e266
Dammalapati <dammalapati@DammalapatisMBP.fios-router.home>
2020-12-17 18:38:57 -0500

    adding imported module to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImportedCommonModule } from 'imported-module/imported-common.module';
 
 const routes: Routes = [];
 
